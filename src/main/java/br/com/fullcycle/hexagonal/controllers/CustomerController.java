d8d3b1224025da2a187fe520b2e5f8d63f8d5bb3
wekers <fernando@wekers.org>
2025-04-10 19:09:10 -0300

    Extraindo os casos de uso implicitos - Refactor CustomerController

--- src/main/java/br/com/fullcycle/hexagonal/controllers/CustomerController.java
+++ src/main/java/br/com/fullcycle/hexagonal/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package br.com.fullcycle.hexagonal.controllers;
 
 import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
 import br.com.fullcycle.hexagonal.application.usecases.CreateCustomerUseCase;
+import br.com.fullcycle.hexagonal.application.usecases.GetCustomerByIdUseCase;
 import br.com.fullcycle.hexagonal.dtos.CustomerDTO;
 import br.com.fullcycle.hexagonal.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+
 
 
 // Adapter
@@ -24,7 +26,6 @@ public class CustomerController {
         try {
             final var useCase = new CreateCustomerUseCase(customerService);
             final var output = useCase.execute(new CreateCustomerUseCase.Input(dto.getCpf(), dto.getEmail(), dto.getName()));
-
             return ResponseEntity.created(URI.create("/customers/" + output.id())).body(output);
         } catch (ValidationException ex){
             return ResponseEntity.unprocessableEntity().body(ex.getMessage());
@@ -34,11 +35,10 @@ public class CustomerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> get(@PathVariable Long id) {
-        var customer = customerService.findById(id);
-        if (customer.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
+        final var useCase = new GetCustomerByIdUseCase(customerService);
+        return useCase.execute(new GetCustomerByIdUseCase.Input(id))
+                .map(ResponseEntity::ok)
+                .orElseGet(ResponseEntity.notFound()::build);
 
-        return ResponseEntity.ok(customer.get());
     }
 }
