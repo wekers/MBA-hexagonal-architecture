b74cfecdd3745be35361ab7008797e58ddbcfadd
wekers <fernando@wekers.org>
2025-04-10 22:30:35 -0300

    Extraindo os casos de uso implicitos - Remove unused imports

--- src/main/java/br/com/fullcycle/hexagonal/application/usecases/GetCustomerByIdUseCase.java
+++ src/main/java/br/com/fullcycle/hexagonal/application/usecases/GetCustomerByIdUseCase.java
@@ -2,7 +2,6 @@ package br.com.fullcycle.hexagonal.application.usecases;
 
 import br.com.fullcycle.hexagonal.application.UseCase;
 import br.com.fullcycle.hexagonal.services.CustomerService;
-import org.springframework.http.ResponseEntity;
 
 import java.util.Objects;
 import java.util.Optional;
