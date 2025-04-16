3fb07f0652b22c975175575384dc52dd0393bace
wekers <fernando@wekers.org>
2025-04-16 18:39:28 -0300

    Injecao de dependencia nos casos de uso e testes integrados - UseCaseConfig @Bean injection

--- src/main/java/br/com/fullcycle/hexagonal/infrastructure/configurations/UseCaseConfig.java
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/configurations/UseCaseConfig.java
@@ -1,22 +1,57 @@
 package br.com.fullcycle.hexagonal.infrastructure.configurations;
 
 
-import br.com.fullcycle.hexagonal.infrastructure.usecases.CreateCustomerUseCase;
+import br.com.fullcycle.hexagonal.infrastructure.services.EventService;
+import br.com.fullcycle.hexagonal.infrastructure.services.PartnerService;
+import br.com.fullcycle.hexagonal.infrastructure.usecases.*;
 import br.com.fullcycle.hexagonal.infrastructure.services.CustomerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
 
 @Configuration
 public class UseCaseConfig {
 
     private final CustomerService customerService;
+    private final EventService eventService;
 
-    public UseCaseConfig(CustomerService customerService) {
-        this.customerService = customerService;
+    private final PartnerService partnerService;
+
+    public UseCaseConfig(final CustomerService customerService, final EventService eventService, final PartnerService partnerService) {
+        this.customerService = Objects.requireNonNull(customerService);
+        this.eventService = Objects.requireNonNull(eventService);
+        this.partnerService = Objects.requireNonNull(partnerService);
     }
 
     @Bean
     public CreateCustomerUseCase createCustomerUseCase() {
         return new CreateCustomerUseCase(customerService);
     }
+
+    @Bean
+    public CreateEventUseCase createEventUseCase() {
+        return new CreateEventUseCase(eventService, partnerService);
+    }
+
+    @Bean
+    public CreatePartnerUseCase createPartnerUseCase() {
+        return new CreatePartnerUseCase(partnerService);
+    }
+
+    @Bean
+    public GetCustomerByIdUseCase getCustomerByIdUseCase() {
+        return new GetCustomerByIdUseCase(customerService);
+    }
+
+    @Bean
+    public GetPartnerByIdUseCase getPartnerByIdUseCase() {
+        return new GetPartnerByIdUseCase(partnerService);
+    }
+
+    @Bean
+    public SubscribeCustomerToEventUseCase subscribeCustomerToEventUseCase() {
+        return new SubscribeCustomerToEventUseCase(customerService, eventService);
+    }
+
 }
