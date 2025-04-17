8a950177e250c84758e66570759be8cd8c55c870
wekers <fernando@wekers.org>
2025-04-17 11:21:23 -0300

    create MainTests

--- src/test/java/br/com/fullcycle/hexagonal/infrastructure/MainTests.java
+++ src/test/java/br/com/fullcycle/hexagonal/infrastructure/MainTests.java
@@ -1,4 +1,4 @@
-package br.com.fullcycle.hexagonal;
+package br.com.fullcycle.hexagonal.infrastructure;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
