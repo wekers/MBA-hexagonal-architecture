392bbb23870946264ccdabf9544ef870d6e0f852
wekers <fernando@wekers.org>
2025-04-17 11:20:02 -0300

    change, create DTO as record

---
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/dtos/NewEventDTO.java
@@ -0,0 +1,13 @@
+package br.com.fullcycle.hexagonal.infrastructure.dtos;
+
+public record NewEventDTO(
+        String name,
+        String date,
+        Integer totalSpots,
+        Long partnerId
+) {
+
+
+
+
+}
---
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/dtos/NewPartnerDTO.java
@@ -0,0 +1,4 @@
+package br.com.fullcycle.hexagonal.infrastructure.dtos;
+
+public record NewPartnerDTO(String cnpj, String email, String name) {
+}
