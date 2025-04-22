35d03ea8a0662ae2b067fd2620a726716b632852
wekers <fernando@wekers.org>
2025-04-22 10:31:05 -0300

    make Partner

---
+++ src/main/java/br/com/fullcycle/hexagonal/application/entities/PartnerId.java
@@ -0,0 +1,27 @@
+package br.com.fullcycle.hexagonal.application.entities;
+
+import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
+
+import java.util.UUID;
+
+public record PartnerId(UUID value) {
+
+    public PartnerId {
+        if (value == null) {
+            throw new ValidationException("Invalid value for PartnerId");
+        }
+    }
+
+    public static PartnerId unique() {
+        return new PartnerId(UUID.randomUUID());
+    }
+
+    public static PartnerId with(final String value) {
+     try {
+         return new PartnerId(UUID.fromString(value));
+     } catch (IllegalArgumentException ex) {
+         throw  new ValidationException("Invalid value for PartnerId");
+     }
+
+    }
+}
---
+++ src/main/java/br/com/fullcycle/hexagonal/application/repositories/PartnerRepository.java
@@ -0,0 +1,19 @@
+package br.com.fullcycle.hexagonal.application.repositories;
+
+import br.com.fullcycle.hexagonal.application.entities.Partner;
+import br.com.fullcycle.hexagonal.application.entities.PartnerId;
+
+import java.util.Optional;
+
+public interface PartnerRepository {
+
+    Optional<Partner> partnerOfId(PartnerId anId);
+
+    Optional<Partner> partnerOfCnpj(String cnpj);
+
+    Optional<Partner> partnerOfEmail(String email);
+
+    Partner create(Partner partner);
+
+    Partner update(Partner partner);
+}
