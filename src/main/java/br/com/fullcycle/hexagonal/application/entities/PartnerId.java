9bfd9117b6178eca8bbdcd6ba9aaceb4c851339b
wekers <fernando@wekers.org>
2025-04-23 10:21:45 -0300

    UUID, Long to String

--- src/main/java/br/com/fullcycle/hexagonal/application/entities/PartnerId.java
+++ src/main/java/br/com/fullcycle/hexagonal/application/entities/PartnerId.java
@@ -4,7 +4,7 @@ import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
 
 import java.util.UUID;
 
-public record PartnerId(UUID value) {
+public record PartnerId(String value) {
 
     public PartnerId {
         if (value == null) {
@@ -13,12 +13,12 @@ public record PartnerId(UUID value) {
     }
 
     public static PartnerId unique() {
-        return new PartnerId(UUID.randomUUID());
+        return new PartnerId(UUID.randomUUID().toString());
     }
 
     public static PartnerId with(final String value) {
      try {
-         return new PartnerId(UUID.fromString(value));
+         return new PartnerId(UUID.fromString(value).toString());
      } catch (IllegalArgumentException ex) {
          throw  new ValidationException("Invalid value for PartnerId");
      }
