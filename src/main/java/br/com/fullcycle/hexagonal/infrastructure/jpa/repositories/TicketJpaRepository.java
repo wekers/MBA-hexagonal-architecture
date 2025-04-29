bd0c2be9083879cb093def4d46e13014556eae22
wekers <fernando@wekers.org>
2025-04-29 16:09:41 -0300

    Long to UUID

--- src/main/java/br/com/fullcycle/hexagonal/infrastructure/jpa/repositories/TicketJpaRepository.java
+++ src/main/java/br/com/fullcycle/hexagonal/infrastructure/jpa/repositories/TicketJpaRepository.java
@@ -3,9 +3,8 @@ package br.com.fullcycle.hexagonal.infrastructure.jpa.repositories;
 import br.com.fullcycle.hexagonal.infrastructure.jpa.entities.TicketEntity;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
+import java.util.UUID;
 
-public interface TicketJpaRepository extends CrudRepository<TicketEntity, Long> {
+public interface TicketJpaRepository extends CrudRepository<TicketEntity, UUID> {
 
-    Optional<TicketEntity> finfByEventIdAndCustomerId(Long eventId, Long customerId);
 }
